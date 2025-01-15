ea9203b66ba30e97e8195c62af4981fd93b2cd5e
matlumin <does_not_matter>
2025-01-15 11:57:14 +0330

    added rate limiter and req.txt

--- server.py
+++ server.py
@@ -2,13 +2,26 @@ import os
 
 from flask import Flask, render_template
 
-
 import list_static_content
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
 
 app:Flask = Flask("some website")
 
 
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
+
+
 @app.route("/")
+@limiter.limit("10/day")
 def index()->str:
 	statics:List[str] = list_static_content.do_it()
 	return render_template("index.html", statics=statics)
